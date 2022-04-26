d2e7540b52ce5061f136358e58ef884b519ba9bb
Bryan Briney <bryanbriney@gmail.com>
2022-04-25 17:45:23 -0700

    add lineage_donut()

--- scab/pl.py
+++ scab/pl.py
@@ -46,7 +46,10 @@ from anndata import AnnData
 
 from natsort import natsorted
 
+from abutils.utils.color import get_cmap
 from abutils.utils.utilities import nested_dict_lookup
+
+
 
 
 
@@ -962,6 +965,276 @@ def germline_use_barplot(adata, gene_names=None, chain='heavy',
         plt.show()
     else:
         return ax
+
+
+
+def lineage_donut(adata, hue=None, palette=None, color=None, cmap=None, name=None, 
+                  hue_order=None, force_categorical_hue=False, lineage_key='lineage', 
+                  figfile=None, figsize=(6, 6), pairs_only=False, 
+                  alt_color='#F5F5F5', edgecolor='white', singleton_color='lightgrey',
+                  shuffle_colors=False, random_seed=1234,
+                  width=0.55, fontsize=28, linewidth=2, text_kws={}, pie_kws={},):
+    '''
+    Creates a donut plot of a population of lineages, with arc widths proportional to lineage size.
+    
+    Args:
+    -----
+    
+        adata (anndata.AnnData): Input ``AnnData`` object. ``adata.obs`` must contain a column for the 
+            lineage name (``lineage_key``) and, optionally, a ``hue`` column.
+            
+        hue (str, dict): Can be either the name of a column in ``adata.obs`` or a ``dict`` mapping
+            lineage names to hue values. Used to determine the color of each lineage arc. If a ``dict`` 
+            is provided, any missing lineage names will still be included in the donut plot but will 
+            be colored using ``alt_color``. There are four possible classes of hue values:  
+             
+                - continuous: hues that map to a continuous numerical space, identified by all ``hue`` 
+                  values being floating point numbers. An example would be log2-transformed
+                  antigen barcode UMI counts. For continuous hues, the mean of all members
+                  in a lineage will be plotted.  
+                  
+                - boolean: hues that map to either ``True`` or ``False``. An example would be specificity
+                  classification. For boolean hues, if any member of a lineage is ``True``, the
+                  entire lineage will be considered ``True``.  
+                  
+                - categorical: hues that map to one of a set of categories. An example would be isotypes. 
+                  For categorical hues, the most common value observed in a lineage will 
+                  be plotted.  
+                  
+            Finally, if ``hue`` is not provided, the lineage name will be considered the ``hue``, and
+            each lineage will be colored separately.
+            
+        palette (dict): A ``dict`` mapping hue categories to colors. For boolean hue types, if ``palette``
+            is not provided, ``color`` will be used for ``True`` and ``alt_color`` will be used for 
+            ``False``. For categorical hue types, if ``color`` is provided, a monochromatic palette 
+            consisting of various shades of ``color`` will be used. If ``color`` is not provided,
+            ``sns.hls_palette()`` will be used to generate a color palette.
+            
+        color (str or list): A color name, hex string or RGB list/tuple for coloring the donut plot. For
+            boolean hue types, ``color`` will be used for ``True``. For categorical and continuous hue
+            types, a monochromatic palette will be created containing various shades of ``color``.
+            
+        alt_color (str or list): A color name, hex string or RGB list/tuple for coloring alternate values 
+            (``False`` boolean hues or values not found in ``palette``). Default is ``'#F5F5F5'``, which is
+            a very light grey.
+        
+        singleton_color (str or list): A color name, hex string or RGB list/tuple for coloring the singleton 
+            arc in the donut plot. Default is ``'lightgrey'``.
+            
+        shuffle_colors (bool): If true, colors will be shuffled prior to assignment to hue categories. This
+            is primarily useful when the hue category is the lineage name and a monochromatic palette is used,  
+            in order to make it easier to distinguish neighboring arcs on the plot. Default is ``False``.
+            
+        name (str): Not used.
+        
+        hue_order (list): A list specifying the hue category order. This does not affect the ordering 
+            of lineages in the donut plot, just the assignment of colors to ``hue`` categories. For example,
+            when plotting with a monochromatic palette (by providing ``color``), ``hue_order`` will
+            order the coloring of ``hue`` categories from dark to light.
+            
+        force_categorical_hue (bool): By default, any ``hue`` categories consisting solely of ``float``s 
+            will be considered continuous and will be colored using a user-supplied colormap (``cmap``) or 
+            with a monochromatic color gradient (using ``color`` as the base color). If ``True``, ``hue``
+            categories will always be considered categorical.
+            
+        lineage_key (str): Column in ``adata.obs`` corresponding to the lineage name. Default is 
+            ``'lineage'``, which is consistent with the default in ``scab.vdj.clonify``.
+            
+        figfile (str): Path to an output figure file. If not provided, the figure will be shown and
+            not saved to file.
+            
+        figsize (list/tuple): Figure size, in inches. Default is ``(6, 6)``.
+        
+        pairs_only (bool): If ``True``, only paired BCRs (containing both heavy and light chains) will
+            be included. Default is ``False``, which plots all BCRs in ``adata``.
+            
+        edgecolor (str or list): A color name, hex string or RGB list/tuple for coloring the edges that divide
+            donut arcs. Default is ``'white'``.
+            
+        random_seed (int, float or str): Used to set ``numpy``'s random seed. Only applicable when 
+            ``shuffle_colors`` is ``True``, and provided mainly to allow users to recreate plots
+            that use shuffled colors (otherwise the shuffle order would be random, thus creating
+            a different plot each time the plottig function is called). Default is ``1234``.
+            
+        width (float): Fraction of the donut plot radius that corresponds to the donut 'hole'. 
+            Default is ``0.55``
+            
+        fontsize (int): Fontsize for the sequence count text displayed in the center of the plot.
+            Default is ``28``.
+            
+        linewidth (float): Width of the lines separating lineage arcs. Default is ``2``.
+        
+        pie_kws (dict): Dictionary containing keyword arguments that will be passed directly to
+            ``ax.pie()``.
+            
+        text_kws (dict): Dictionary containing keyword arguments that will be passed directly to
+            ``ax.text()`` when drawing the text in the center of the plot.
+            
+    Returns:
+    --------
+    
+    Nothing is retured. If ``figfile`` is supplied, the figure is saved to file, otherwise the figure is shown.
+        
+    
+    For continuous hues (for example, AgBC UMI counts), the mean value for each lineage is used. 
+    For boolean values (for example, specificity classifications), the lineage is considered ``True`` if
+    any lineage member is ``True``. For categorical values (for example, CDR3 length), the most common
+    value for each lineage is used.
+    '''
+    adata = adata.copy()
+    if pairs_only:
+        adata = adata[[b.is_pair for b in adata.obs.bcr]]
+    
+    # organize linages into a DataFrame
+    ldata = []
+    singleton_count = 0
+    for i, (l, s) in enumerate(adata.obs[lineage_key].value_counts().items()):
+        if s > 1:
+            ldata.append({'lineage': l, 'size': s, 'order': i})
+        else:
+            singleton_count += 1
+    df = pd.DataFrame(ldata)
+    
+    # singletons
+    singleton_df = pd.DataFrame([{'lineage': 'singletons',
+                                 'size': singleton_count,
+                                 'order': df.shape[0] + 1,
+                                 'hue': 'singletons',
+                                 'color': singleton_color}, ])
+    
+    # hue
+    if hue is not None:
+        if isinstance(hue, dict):
+            _hue = [hue.get(l, None) for l in df['lineage']]
+            df['hue'] = _hue
+        elif hue in adata.obs:
+            if all([isinstance(h, float) for h in adata.obs[hue]]):
+                _hue = []
+                for l in df['lineage']:
+                    _adata = adata[adata.obs[lineage_key] == l]
+                    if _adata:
+                        h = np.mean(_adata.obs[hue])
+                        _hue.append(h)
+                    else:
+                        _hue.append(None)
+                df['hue'] = _hue
+            elif all([isinstance(h, bool) for h in adata.obs[hue]]):
+                _hue = []
+                for l in df['lineage']:
+                    _adata = adata[adata.obs[lineage_key] == l]
+                    if _adata:
+                        h = any(_adata.obs[hue])
+                        _hue.append(h)
+                    else:
+                        _hue.append(None)
+                df['hue'] = _hue
+            else:
+                _hue = []
+                for l in df['lineage']:
+                    _adata = adata[adata.obs[lineage_key] == l]
+                    if _adata:
+                        h = _adata.obs[hue].value_counts().index[0]
+                        _hue.append(h)
+                    else:
+                        _hue.append(None)
+                df['hue'] = _hue
+        else:
+            err = "\nERROR: hue must either be the name of a column in adata.obs or a dictionary "
+            err += f"mapping lineage names to hue values. You provided {hue}.\n"
+            print(err)
+            sys.exit()
+    else:
+        df['hue'] = df['lineage']
+    # set hue type
+    if all([isinstance(h, float) for h in df['hue']]) and not force_categorical_hue:
+        hue_type = 'continuous'
+    elif all([isinstance(h, bool) for h in df['hue']]):
+        hue_type = 'boolean'
+    elif all([h in df['lineage'] for h in df['hue']]):
+        hue_type = 'lineage'
+    else:
+        hue_type = 'categorical'
+            
+    # color
+    if hue_type == 'continuous':
+        if cmap is None:
+            color = color if color is not None else sns.color_palette()[0]
+        cmap = get_cmap(from_color=color) if cmap is None else get_cmap(cmap)
+        norm_hue = (df['hue'] - df['hue'].min()) / (df['hue'].max() - df['hue'].min())
+        df['color'] = [cmap(nh) for nh in norm_hue]
+    elif hue_type == 'boolean':
+        if palette is None:
+            color = color if color is not None else sns.color_palette()[0]
+            pos = color
+            neg = alt_color
+        else:
+            pos = palette[True]
+            neg = palette[False]
+        df['color'] = [pos if h else neg for h in df['hue']]
+    else:
+        if palette is None:
+            if hue_type == 'lineage':
+                hue_order = df['hue']
+            else:
+                hue_order = hue_order if hue_order is not None else natsorted(df['hue'].dropna().unique())
+            if color is not None:
+                colors = _get_monochrome_colors(color, len(hue_order))
+                if shuffle_colors:
+                    primary = colors[0]
+                    secondary = colors[1:]
+                    np.random.seed(random_seed)
+                    np.random.shuffle(secondary)
+                    colors = [primary] + secondary
+            else:
+                colors = sns.hls_palette(n_colors=len(hue_order))
+            palette = {h: c for h, c in zip(hue_order, colors)}
+        df['color'] = [palette.get(h, alt_color) for h in df['hue']]        
+    
+    # concat the singletons and sort
+    df = pd.concat([df, singleton_df], ignore_index=True)
+    df = df.sort_values(by='order', ascending=False)
+    
+    # make the plot
+    plt.figure(figsize=figsize)
+    ax = plt.gca()
+    ax.axis('equal')
+    pctdistance = 1 - width / 2
+    pie_kwargs = dict(startangle=90,
+                      radius=1,
+                      pctdistance=1-width/2)
+    for k, v in pie_kws.items():
+        kwargs[k] = v
+    pie_kwargs['colors'] = df['color']
+    slices, _ = ax.pie(df['size'], **pie_kwargs)
+    plt.setp(slices, width=width, edgecolor=edgecolor)
+    for w in slices:
+        w.set_linewidth(linewidth)
+    # add text to the center of the donut (total sequence count)
+    txt_kwargs = dict(size=fontsize, color='k', va='center', fontweight='bold')
+    for k, v in text_kws.items():
+        txt_kwargs[k] = v
+    ax.text(0, 0, str(adata.shape[0]), ha='center', **txt_kwargs)
+    plt.tight_layout()
+
+    if figfile is not None:
+        plt.savefig(figfile)
+    else:
+        plt.show()            
+
+
+def _get_monochrome_colors(monochrome_color, N):
+    cmap = get_cmap(from_color=monochrome_color)
+    # this is a bit convoluted, but what's happening is we're getting different colormap
+    # values (which range from 0 to 1). Calling cmap(i) returns an rgba tuple, but we just need
+    # the rbg, so we drop the a. To make sure that one of the colors isn't pure white,
+    # we ask np.linspace() for one more value than we need, reverse the list of RGB tuples
+    # so that it goes from dark to light, and drop the lightest value
+    RGB_tuples = [cmap(i)[:-1] for i in np.linspace(0, 1, N + 1)][::-1][:-1]
+    return RGB_tuples
+
+
+
+
 
 
 
